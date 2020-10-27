c36c966ea9f985e12e85f878c14d07a747984672
jakuantayper <66756992+jakuantayper@users.noreply.github.com>
2020-10-26 20:23:02 -0600

    1

---
+++ Sales.Common/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace Sales.Common.Models
+{
+using System;
+using System.ComponentModel.DataAnnotations;
+   public  class Product
+    {
+
+        [Key]
+        public int Productid { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public Decimal Price { get; set; }
+        public int IsAvailable { get; set; }
+        public DateTime PublishOn { get; set; }
+        public override string ToString()
+        {
+            return this.Description;
+        }
+
+    }
+}
---
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,57 @@
+﻿namespace Sales.ViewModels
+{
+    using Common.Models;
+    using GalaSoft.MvvmLight.Command;
+    using Services;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
+    public class ProductsViewModel : BaseViewModel 
+    {
+        private APIService apiService;
+
+        private bool isRefreshing;
+
+        private ObservableCollection<Product> products;
+        public ObservableCollection<Product> Products 
+        {
+            get { return this.products; }
+            set { this.SetValue(ref this.products, value);}
+        }
+
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { this.SetValue(ref this.isRefreshing, value); }
+        }
+        public ProductsViewModel()
+        {
+            this.apiService = new APIService();
+            this.LoadProducts();
+        }
+        private async void LoadProducts()
+        {
+            this.IsRefreshing = true;
+            var response = await this.apiService.GetList<Product>("http://jakuantayper-001-site10.atempurl.com", "/api", "/Products");
+            if (!response.IsSuccess)
+            {
+                this.IsRefreshing = false;
+                await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
+                return;
+            }
+            
+            var List = (List<Product>)response.Result;
+            this.Products = new ObservableCollection<Product>(List);
+            this.IsRefreshing = false;
+        }
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new RelayCommand(LoadProducts);
+            }
+        }
+    }
+}
